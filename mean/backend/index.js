63f122d7faee9bc6d120603755c3dfd3b91df126
riteshsutariya <riteshsutariya94@gmail.com>
2022-11-03 15:39:32 +0530

    backend ready for products CRUD in shopping cart(mean)

---
+++ mean/backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+require('dotenv').config()
+//database
+const database = require('./config/db')
+
+//req body,json configuration
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//product route
+const productRoute = require('./routes/product')
+const PORT = process.env.PORT;
+
+app.use('/product', productRoute)
+
+app.get('/', (req, res) => {
+    res.status(200).json({ response: { message: 'api running.' } })
+})
+
+app.listen(PORT, () => {
+    console.log(`api running on http://localhost:${PORT}`)
+})
