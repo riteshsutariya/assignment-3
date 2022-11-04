610480a7b96b5f3e15dfad7bd34a8c33ddef52b7
riteshsutariya <riteshsutariya94@gmail.com>
2022-11-04 07:34:44 +0530

    completed front-end for cart management sysrtem

--- mean/front-web/src/app/app-routing.module.ts
+++ mean/front-web/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ProductsComponent } from './products/products.component';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
+import { ProductUpdateComponent } from './product-update/product-update.component';
+import { ProductDeleteComponent } from './product-delete/product-delete.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/product', pathMatch: 'full' },
+  {
+   path:'product',
+   component:ProductsComponent,
+   pathMatch:'full'
+  },
+  {
+    path:'product/add',
+    component:ProductInsertComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'product/update/:id',
+    component:ProductUpdateComponent
+  },
+  {
+    path:'product/delete/:id',
+    component:ProductDeleteComponent
+  },
+  {
+    path:'product/manage',
+    component:ProductManagerComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mean/front-web/src/app/app.module.ts
+++ mean/front-web/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
+import { ProductUpdateComponent } from './product-update/product-update.component';
+import { ProductDeleteComponent } from './product-delete/product-delete.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+import { ProductManagerComponent } from './product-manager/product-manager.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductInsertComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent,
+    ProductManagerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
